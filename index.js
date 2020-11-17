1111cb03f0a0b7a857001a43ddf7193a93d5fd39
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2020-11-17 17:31:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ function wakeDog(dogName,dogBreed){
  return `Wake ${dogName} the ${dogBreed}`;
 }
 function leashDog(dogName,dogBreed){
-  return console.log(`Leash ${dogName} the ${dogBreed}`);
+  return `Leash ${dogName} the ${dogBreed}`;
 }
 function walkToPark(dogName,dogBreed){
-  return console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  return `Walk to the park with ${dogName} the ${dogBreed}`;
 }
 function throwFrisbee(dogName,dogBreed){
- return console.log(`Throw the frisbeefor ${dogName} the ${dogBreed}`);
+ return `Throw the frisbeefor ${dogName} the ${dogBreed}`;
 }
 function walkHome(dogName,dogBreed){
-return  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+return `Walk home with ${dogName} the ${dogBreed}`;
 }
 function unleashDog(dogName,dogBreed){
-  return console.log(`Unleash ${dogName} the ${dogBreed}`);
+  return `Unleash ${dogName} the ${dogBreed}`;
 }
 let routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog];
  function exerciseDog(dogName, dogBreed){
