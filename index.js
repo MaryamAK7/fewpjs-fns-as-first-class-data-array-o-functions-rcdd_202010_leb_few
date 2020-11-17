952032ddb2bcf5f9bc5ebed1c1a559b06dab04dc
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2020-11-17 17:22:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 function wakeDog(dogName,dogBreed){
   console.log(`wake ${dogName} the ${dogBreed}`);
 }
+function leashDog(dogName,dogBreed){
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+}
+function walkToPark(dogName,dogBreed){
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+}
+function throwFrisbee(dogName,dogBreed){
+  console.log(`Throw the frisbeefor ${dogName} the ${dogBreed}`);
+}
+function walkHome(dogName,dogBreed){
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+}
+function unleashDog(dogName,dogBreed){
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
+}
+let routine = [wakeDog(),leashDog(),walkToPark(),throwFrisbee(),walkHome(),unleashDog()];
+
+
+
+
